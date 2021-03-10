d9baa0a7c76db45cf5f6a62b01075f7ac4c3a7ba
amandarosa15 <79384892+amandarosa15@users.noreply.github.com>
2021-03-10 15:39:51 -0600

    Form 4

---
+++ forms/ECnestedLoops/ECnestedLoops.js
@@ -0,0 +1,5 @@
+Let rows = promp("Enter the amount of rows (Choose between 1-10)")
+Let columns = promp("Enter the amount of columns (Choose between 1-10)")
+//if (rows>10 || rows<0)
+  //alert("You enter an amount of rows outside the range")
+  
---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,8 @@
+/*let password=prompt("Enter your new password")
+let doublecheck=prompt("Enter your password again")
+var result = password.localeCompare(doublecheck)
+if (result==1)
+  console.log("The passwords are not the same. ")
+if(result==0)
+  console.log("The passwords are the same. ")
+*/
