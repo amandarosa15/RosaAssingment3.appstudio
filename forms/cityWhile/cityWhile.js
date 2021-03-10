4472fe89eda30aff392bd034e2dfe656ca2afe34
amandarosa15 <79384892+amandarosa15@users.noreply.github.com>
2021-03-10 10:07:02 -0600

    Second Form

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,15 @@
+/*
+var cities =[]
+var city = prompt("Enter a city, say stop when you are ready")
+while (city != "stop"){
+  cities.push(city.toLowerCase())
+  city = prompt("Enter a city, say stop when you are ready")
+  }
+console.log(cities)
+let num=0
+while (num>cities.length){
+    result=cities[num]
+    console.log(result)
+    n=n+1
+    }
+    */
