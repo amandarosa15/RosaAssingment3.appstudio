21d820f6679b117122040adea2c34190c598dc54
amandarosa15 <79384892+amandarosa15@users.noreply.github.com>
2021-03-09 21:11:58 -0600

    Create cityWhile.js

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,13 @@
+var cities =[]
+var city = prompt("Enter a city, say stop when you are ready")
+while (city != "stop"){
+  cities.push(city.toLowerCase())
+  city = prompt("Enter a city, say stop when you are ready")
+  }
+console.log(cities)
+let num=0
+while (num>cities.length){
+    result=cities[num]
+    console.log(`${result} \n`) 
+    n=n+1
+    }
